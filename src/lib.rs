b959eeaf9ab755311801bdf9637d411c200459d7
James Walker <james.chunho@gmail.com>
2024-08-01 02:49:59 +0800

    copy gui example from gain plugin

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ use biquad::Precision;
 use envelope::Envelope;
 use filters::{ButterworthLPF, FirstOrderLPF, LinkwitzRileyHPF, LinkwitzRileyLPF};
 use nih_plug::{buffer::ChannelSamples, prelude::*};
+use nih_plug_egui::{create_egui_editor, egui, widgets, EguiState};
 use parameter_formatters::{s2v_f32_ms_then_s, v2s_f32_ms_then_s};
 use ringbuffer::{AllocRingBuffer, RingBuffer};
 use std::sync::Arc;
@@ -27,6 +28,17 @@ struct SaiSampler {
     buf: AllocRingBuffer<f32>,
     env: Option<Envelope>,
     env_filter: FirstOrderLPF,
+
+    /// The editor state, saved together with the parameter state so the custom scaling can be
+    /// restored.
+    // #[persist = "editor-state"]
+    editor_state: Arc<EguiState>,
+    /// The current data for the peak meter. This is stored as an [`Arc`] so we can share it between
+    /// the GUI and the audio processing parts. If you have more state to share, then it's a good
+    /// idea to put all of that in a struct behind a single `Arc`.
+    ///
+    /// This is stored as voltage gain.
+    peak_meter: Arc<AtomicF32>,
 }
 
 impl Default for SaiSampler {
@@ -44,6 +56,9 @@ impl Default for SaiSampler {
             buf: AllocRingBuffer::new(1),
             env: None,
             env_filter: FirstOrderLPF::new(0.0, 0.0),
+            // TEMP
+            peak_meter: Arc::new(AtomicF32::new(util::MINUS_INFINITY_DB)),
+            editor_state: EguiState::from_size(300, 400),
         }
     }
 }
@@ -205,6 +220,9 @@ impl Plugin for SaiSampler {
         let mut next_event = ctx.next_event();
 
         for (sample_id, mut channel_samples) in buffer.iter_samples().enumerate() {
+            // GUI-specific variables
+            let mut amplitude = 0.0;
+
             // update params
             let gain_reduction_db = gain_to_db(self.params.gain_reduction.smoothed.next());
             let precomp = self.params.precomp.smoothed.next() / 1000.0;
@@ -301,11 +319,108 @@ impl Plugin for SaiSampler {
             env_val = self.env_filter.process_sample(env_val as Precision) as f32;
             for sample in channel_samples {
                 *sample = *sample * db_to_gain((env_val as f32) * gain_reduction_db);
+                amplitude += sample.abs();
                 // *sample = *sample * env_val as f32;
+            }
+
+            // GUI-specific code
+            if self.editor_state.is_open() {
+                // divide by 2 channels
+                amplitude = amplitude / 2.0;
+                let current_peak_meter = self.peak_meter.load(std::sync::atomic::Ordering::Relaxed);
+                const PEAK_METER_DECAY_MS: f64 = 150.0;
+                let peak_meter_decay_weight =
+                    0.25f64.powf((self.sr as f64 * PEAK_METER_DECAY_MS / 1000.0).recip()) as f32;
+                let new_peak_meter = if amplitude > current_peak_meter {
+                    amplitude
+                } else {
+                    current_peak_meter * peak_meter_decay_weight
+                        + amplitude * (1.0 - peak_meter_decay_weight)
+                };
+
+                self.peak_meter
+                    .store(new_peak_meter, std::sync::atomic::Ordering::Relaxed)
             }
         }
 
         ProcessStatus::Normal
+    }
+
+    fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+        let params = self.params.clone();
+        let peak_meter = self.peak_meter.clone();
+        create_egui_editor(
+            self.editor_state.clone(),
+            (),
+            |_, _| {},
+            move |egui_ctx, setter, _state| {
+                egui::CentralPanel::default().show(egui_ctx, |ui| {
+                    // NOTE: See `plugins/diopser/src/editor.rs` for an example using the generic UI widget
+
+                    // TODO: Add a proper custom widget instead of reusing a progress bar
+                    let peak_meter =
+                        util::gain_to_db(peak_meter.load(std::sync::atomic::Ordering::Relaxed));
+                    let peak_meter_text = if peak_meter > util::MINUS_INFINITY_DB {
+                        format!("{peak_meter:.1} dBFS")
+                    } else {
+                        String::from("-inf dBFS")
+                    };
+
+                    let peak_meter_normalized = (peak_meter + 60.0) / 60.0;
+                    ui.allocate_space(egui::Vec2::splat(2.0));
+                    ui.add(
+                        egui::widgets::ProgressBar::new(peak_meter_normalized)
+                            .text(peak_meter_text),
+                    );
+
+                    // This is a fancy widget that can get all the information it needs to properly
+                    // display and modify the parameter from the parametr itself
+                    // It's not yet fully implemented, as the text is missing.
+                    ui.label("gain_reduction");
+                    ui.add(widgets::ParamSlider::for_param(
+                        &params.gain_reduction,
+                        setter,
+                    ));
+                    ui.label("precomp");
+                    ui.add(widgets::ParamSlider::for_param(&params.precomp, setter));
+                    ui.label("release");
+                    ui.add(widgets::ParamSlider::for_param(&params.release, setter));
+                    ui.label("low_crossover");
+                    ui.add(widgets::ParamSlider::for_param(
+                        &params.low_crossover,
+                        setter,
+                    ));
+                    ui.label("high_crossover");
+                    ui.add(widgets::ParamSlider::for_param(
+                        &params.high_crossover,
+                        setter,
+                    ));
+
+                    // ui.label(
+                    //     "Also gain, but with a lame widget. Can't even render the value correctly!",
+                    // );
+                    // // This is a simple naieve version of a parameter slider that's not aware of how
+                    // // the parameters work
+                    // ui.add(
+                    //     egui::widgets::Slider::from_get_set(-30.0..=30.0, |new_value| {
+                    //         match new_value {
+                    //             Some(new_value_db) => {
+                    //                 let new_value = util::gain_to_db(new_value_db as f32);
+
+                    //                 setter.begin_set_parameter(&params.gain);
+                    //                 setter.set_parameter(&params.gain, new_value);
+                    //                 setter.end_set_parameter(&params.gain);
+
+                    //                 new_value_db
+                    //             }
+                    //             None => util::gain_to_db(params.gain.value()) as f64,
+                    //         }
+                    //     })
+                    //     .suffix(" dB"),
+                    // );
+                });
+            },
+        )
     }
 }
 
