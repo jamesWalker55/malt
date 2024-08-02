46c625a23fdf4971c05812c16d9da13cc0942b28
James Walker <james.chunho@gmail.com>
2024-08-02 17:11:32 +0800

    New gain filter

--- src/svf.rs
+++ src/svf.rs
@@ -134,6 +134,93 @@ impl<T: GainlessFilterKind> GainlessFilter<T> {
     }
 }
 
+pub(crate) trait GainFilterKind {
+    fn coefficients(f: Precision, q: Precision, gain: Precision, sr: Precision) -> [Precision; 6];
+}
+
+/// E.g. bell, low-shelf, high-shelf
+pub(crate) struct GainFilter<T: GainFilterKind> {
+    svf: Svf,
+    f: Precision,
+    q: Precision,
+    gain: Precision,
+    sr: Precision,
+    kind: std::marker::PhantomData<T>,
+}
+
+impl<T: GainFilterKind> GainFilter<T> {
+    pub(crate) fn process_sample(&mut self, x0: Precision) -> Precision {
+        self.svf.process_sample(x0)
+    }
+
+    pub(crate) fn new(
+        frequency: Precision,
+        q: Precision,
+        gain: Precision,
+        sample_rate: Precision,
+    ) -> Self {
+        let coeffs = T::coefficients(frequency, q, gain, sample_rate);
+        Self {
+            svf: Svf::new(
+                coeffs[0], coeffs[1], coeffs[2], coeffs[3], coeffs[4], coeffs[5],
+            ),
+            f: frequency,
+            gain,
+            q,
+            sr: sample_rate,
+            kind: std::marker::PhantomData,
+        }
+    }
+
+    pub(crate) fn set_frequency(&mut self, f: Precision) {
+        if f == self.f {
+            return;
+        }
+
+        self.f = f;
+        let coeffs = T::coefficients(f, self.q, self.gain, self.sr);
+        self.svf.set_coefficients(
+            coeffs[0], coeffs[1], coeffs[2], coeffs[3], coeffs[4], coeffs[5],
+        );
+    }
+
+    pub(crate) fn set_q(&mut self, q: Precision) {
+        if q == self.q {
+            return;
+        }
+
+        self.q = q;
+        let coeffs = T::coefficients(self.f, q, self.gain, self.sr);
+        self.svf.set_coefficients(
+            coeffs[0], coeffs[1], coeffs[2], coeffs[3], coeffs[4], coeffs[5],
+        );
+    }
+
+    pub(crate) fn set_gain(&mut self, gain: Precision) {
+        if gain == self.gain {
+            return;
+        }
+
+        self.gain = gain;
+        let coeffs = T::coefficients(self.f, self.q, gain, self.sr);
+        self.svf.set_coefficients(
+            coeffs[0], coeffs[1], coeffs[2], coeffs[3], coeffs[4], coeffs[5],
+        );
+    }
+
+    pub(crate) fn set_sample_rate(&mut self, sr: Precision) {
+        if sr == self.sr {
+            return;
+        }
+
+        self.sr = sr;
+        let coeffs = T::coefficients(self.f, self.q, self.gain, sr);
+        self.svf.set_coefficients(
+            coeffs[0], coeffs[1], coeffs[2], coeffs[3], coeffs[4], coeffs[5],
+        );
+    }
+}
+
 pub(crate) struct LowPass;
 
 impl GainlessFilterKind for LowPass {
@@ -231,6 +318,60 @@ impl GainlessFilterKind for AllPass {
         let m0 = 1.0;
         let m1 = -2.0 * k;
         let m2 = 0.0;
+
+        [a1, a2, a3, m0, m1, m2]
+    }
+}
+
+pub(crate) struct Bell;
+
+impl GainFilterKind for Bell {
+    fn coefficients(f: Precision, q: Precision, gain: Precision, sr: Precision) -> [Precision; 6] {
+        let a = (gain).sqrt();
+        let g = (C::PI * f / sr).tan();
+        let k = 1.0 / (q * a);
+        let a1 = 1.0 / (1.0 + g * (g + k));
+        let a2 = g * a1;
+        let a3 = g * a2;
+        let m0 = 1.0;
+        let m1 = k * (a * a - 1.0);
+        let m2 = 0.0;
+
+        [a1, a2, a3, m0, m1, m2]
+    }
+}
+
+pub(crate) struct LowShelf;
+
+impl GainFilterKind for LowShelf {
+    fn coefficients(f: Precision, q: Precision, gain: Precision, sr: Precision) -> [Precision; 6] {
+        let a = (gain).sqrt();
+        let g = (C::PI * f / sr).tan() / (a).sqrt();
+        let k = 1.0 / q;
+        let a1 = 1.0 / (1.0 + g * (g + k));
+        let a2 = g * a1;
+        let a3 = g * a2;
+        let m0 = 1.0;
+        let m1 = k * (a - 1.0);
+        let m2 = a * a - 1.0;
+
+        [a1, a2, a3, m0, m1, m2]
+    }
+}
+
+pub(crate) struct HighShelf;
+
+impl GainFilterKind for HighShelf {
+    fn coefficients(f: Precision, q: Precision, gain: Precision, sr: Precision) -> [Precision; 6] {
+        let a = (gain).sqrt();
+        let g = (C::PI * f / sr).tan() * (a).sqrt();
+        let k = 1.0 / q;
+        let a1 = 1.0 / (1.0 + g * (g + k));
+        let a2 = g * a1;
+        let a3 = g * a2;
+        let m0 = a * a;
+        let m1 = k * (1.0 - a) * a;
+        let m2 = 1.0 - a * a;
 
         [a1, a2, a3, m0, m1, m2]
     }
