23c8ad23c7d5b5045548c477194e34bcf84afe59
James Walker <james.chunho@gmail.com>
2024-08-03 20:53:47 +0800

    test the other splitter

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ use nih_plug::{buffer::ChannelSamples, prelude::*};
 use nih_plug_egui::{create_egui_editor, egui, widgets, EguiState};
 use parameter_formatters::{s2v_f32_ms_then_s, v2s_f32_ms_then_s};
 use ringbuffer::{AllocRingBuffer, RingBuffer};
-use splitter::MinimumTwoBand24Slope;
+use splitter::MinimumTwoBand12Slope;
 use std::sync::Arc;
 use util::{db_to_gain, gain_to_db};
 
@@ -24,8 +24,8 @@ pub struct SaiSampler {
     sr: f32,
     latency_seconds: f32,
     latency_samples: u32,
-    splitter_l: MinimumTwoBand24Slope,
-    splitter_r: MinimumTwoBand24Slope,
+    splitter_l: MinimumTwoBand12Slope,
+    splitter_r: MinimumTwoBand12Slope,
     buf: AllocRingBuffer<f32>,
     env: Option<Envelope>,
     env_filter: FixedQFilter<FirstOrderLP>,
@@ -50,8 +50,8 @@ impl Default for SaiSampler {
             sr: 0.0,
             latency_seconds: 0.0,
             latency_samples: 0,
-            splitter_l: MinimumTwoBand24Slope::new(0.0, 0.0),
-            splitter_r: MinimumTwoBand24Slope::new(0.0, 0.0),
+            splitter_l: MinimumTwoBand12Slope::new(0.0, 0.0),
+            splitter_r: MinimumTwoBand12Slope::new(0.0, 0.0),
             buf: AllocRingBuffer::new(1),
             env: None,
             env_filter: FixedQFilter::new(0.0, 0.0),
@@ -191,8 +191,8 @@ impl Plugin for SaiSampler {
         };
 
         // setup filters
-        self.splitter_l = MinimumTwoBand24Slope::new(1000.0, self.sr.into());
-        self.splitter_r = MinimumTwoBand24Slope::new(1000.0, self.sr.into());
+        self.splitter_l = MinimumTwoBand12Slope::new(1000.0, self.sr.into());
+        self.splitter_r = MinimumTwoBand12Slope::new(1000.0, self.sr.into());
 
         // clear envelope
         self.env = None;
