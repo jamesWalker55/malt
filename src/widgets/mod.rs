b8c3f75efab82d11d719af59485a1ea89f5f803d
James Walker <james.chunho@gmail.com>
2024-08-06 12:05:18 +0800

    Merge structs ArcKnob and SliderRegion

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -23,148 +23,33 @@ static DRAG_NORMALIZED_START_VALUE_MEMORY_ID: Lazy<egui::Id> =
     Lazy::new(|| egui::Id::new((file!(), 0)));
 static DRAG_AMOUNT_MEMORY_ID: Lazy<egui::Id> = Lazy::new(|| egui::Id::new((file!(), 1)));
 
-struct SliderRegion<'a, P: Param> {
-    param: &'a P,
-    param_setter: &'a ParamSetter<'a>,
+pub(crate) fn get_drag_normalized_start_value_memory(ui: &Ui) -> f32 {
+    ui.memory(|mem| mem.data.get_temp(*DRAG_NORMALIZED_START_VALUE_MEMORY_ID))
+        .unwrap_or(0.5)
 }
 
-impl<'a, P: Param> SliderRegion<'a, P> {
-    fn new(param: &'a P, param_setter: &'a ParamSetter) -> Self {
-        SliderRegion {
-            param,
-            param_setter,
-        }
-    }
+pub(crate) fn set_drag_normalized_start_value_memory(ui: &Ui, amount: f32) {
+    ui.memory_mut(|mem| {
+        mem.data
+            .insert_temp(*DRAG_NORMALIZED_START_VALUE_MEMORY_ID, amount)
+    });
+}
 
-    fn set_normalized_value(&self, normalized: f32) {
-        // This snaps to the nearest plain value if the parameter is stepped in some way.
-        // TODO: As an optimization, we could add a `const CONTINUOUS: bool` to the parameter to
-        //       avoid this normalized->plain->normalized conversion for parameters that don't need
-        //       it
-        let value = self.param.preview_plain(normalized);
-        if value != self.plain_value() {
-            self.param_setter.set_parameter(self.param, value);
-        }
-    }
+pub(crate) fn get_drag_amount_memory(ui: &Ui) -> f32 {
+    ui.memory(|mem| mem.data.get_temp(*DRAG_AMOUNT_MEMORY_ID))
+        .unwrap_or(0.0)
+}
 
-    fn plain_value(&self) -> P::Plain {
-        self.param.modulated_plain_value()
-    }
-
-    fn normalized_value(&self) -> f32 {
-        self.param.modulated_normalized_value()
-    }
-
-    fn get_drag_normalized_start_value_memory(ui: &Ui) -> f32 {
-        ui.memory(|mem| mem.data.get_temp(*DRAG_NORMALIZED_START_VALUE_MEMORY_ID))
-            .unwrap_or(0.5)
-    }
-
-    fn set_drag_normalized_start_value_memory(ui: &Ui, amount: f32) {
-        ui.memory_mut(|mem| {
-            mem.data
-                .insert_temp(*DRAG_NORMALIZED_START_VALUE_MEMORY_ID, amount)
-        });
-    }
-
-    fn get_drag_amount_memory(ui: &Ui) -> f32 {
-        ui.memory(|mem| mem.data.get_temp(*DRAG_AMOUNT_MEMORY_ID))
-            .unwrap_or(0.0)
-    }
-
-    fn set_drag_amount_memory(ui: &Ui, amount: f32) {
-        ui.memory_mut(|mem| mem.data.insert_temp(*DRAG_AMOUNT_MEMORY_ID, amount));
-    }
-
-    /// Begin and end drag still need to be called when using this..
-    fn reset_param(&self) {
-        self.param_setter
-            .set_parameter(self.param, self.param.default_plain_value());
-    }
-
-    fn granular_drag(&self, ui: &Ui, drag_delta: Vec2) {
-        // Remember the intial position when we started with the granular drag. This value gets
-        // reset whenever we have a normal itneraction with the slider.
-        let start_value = if Self::get_drag_amount_memory(ui) == 0.0 {
-            Self::set_drag_normalized_start_value_memory(ui, self.normalized_value());
-            self.normalized_value()
-        } else {
-            Self::get_drag_normalized_start_value_memory(ui)
-        };
-
-        let total_drag_distance = -drag_delta.y + Self::get_drag_amount_memory(ui);
-        Self::set_drag_amount_memory(ui, total_drag_distance);
-
-        self.set_normalized_value(
-            (start_value + (total_drag_distance * GRANULAR_DRAG_MULTIPLIER)).clamp(0.0, 1.0),
-        );
-    }
-
-    // Copied this to modify the normal drag behavior to not match a slider
-    fn normal_drag(&self, ui: &Ui, drag_delta: Vec2) {
-        let start_value = if Self::get_drag_amount_memory(ui) == 0.0 {
-            Self::set_drag_normalized_start_value_memory(ui, self.normalized_value());
-            self.normalized_value()
-        } else {
-            Self::get_drag_normalized_start_value_memory(ui)
-        };
-
-        let total_drag_distance = -drag_delta.y + Self::get_drag_amount_memory(ui);
-        Self::set_drag_amount_memory(ui, total_drag_distance);
-
-        self.set_normalized_value(
-            (start_value + (total_drag_distance * NORMAL_DRAG_MULTIPLIER)).clamp(0.0, 1.0),
-        );
-    }
-
-    // Handle the input for a given response. Returns an f32 containing the normalized value of
-    // the parameter.
-    fn handle_response(&self, ui: &Ui, response: &mut Response) -> f32 {
-        // This has been replaced with the ParamSlider/CustomParamSlider structure and supporting
-        // functions (above) since that was still working in egui 0.22
-
-        if response.drag_started() {
-            // When beginning a drag or dragging normally, reset the memory used to keep track of
-            // our granular drag
-            self.param_setter.begin_set_parameter(self.param);
-            Self::set_drag_amount_memory(ui, 0.0);
-        }
-        if let Some(_clicked_pos) = response.interact_pointer_pos() {
-            if ui.input(|mem| mem.modifiers.command) {
-                // Like double clicking, Ctrl+Click should reset the parameter
-                self.reset_param();
-                response.mark_changed();
-            } else if ui.input(|mem| mem.modifiers.shift) {
-                // And shift dragging should switch to a more granular input method
-                self.granular_drag(ui, response.drag_delta());
-                response.mark_changed();
-            } else {
-                self.normal_drag(ui, response.drag_delta());
-                response.mark_changed();
-                //Self::set_drag_amount_memory(ui, 0.0);
-            }
-        }
-        if response.double_clicked() {
-            self.reset_param();
-            response.mark_changed();
-        }
-        if response.drag_stopped() {
-            self.param_setter.end_set_parameter(self.param);
-            Self::set_drag_amount_memory(ui, 0.0);
-        }
-        self.normalized_value()
-    }
-
-    fn get_string(&self) -> String {
-        self.param.to_string()
-    }
+pub(crate) fn set_drag_amount_memory(ui: &Ui, amount: f32) {
+    ui.memory_mut(|mem| mem.data.insert_temp(*DRAG_AMOUNT_MEMORY_ID, amount));
 }
 
 pub(crate) struct ArcKnob<'a, P: Param> {
     size: f32,
     line_width: f32,
-    slider_region: SliderRegion<'a, P>,
     highlight_color: Color32,
+    param: &'a P,
+    param_setter: &'a ParamSetter<'a>,
 }
 
 impl<'a, P: Param> ArcKnob<'a, P> {
@@ -187,19 +72,107 @@ impl<'a, P: Param> ArcKnob<'a, P> {
         ArcKnob {
             size,
             line_width,
-            slider_region: SliderRegion::new(param, param_setter),
             highlight_color,
+            param,
+            param_setter,
         }
+    }
+
+    fn set_normalized_value(&self, normalized: f32) {
+        // This snaps to the nearest plain value if the parameter is stepped in some way.
+        // TODO: As an optimization, we could add a `const CONTINUOUS: bool` to the parameter to
+        //       avoid this normalized->plain->normalized conversion for parameters that don't need
+        //       it
+        let value = self.param.preview_plain(normalized);
+        if value != self.param.modulated_plain_value() {
+            self.param_setter.set_parameter(self.param, value);
+        }
+    }
+
+    fn normalized_value(&self) -> f32 {
+        self.param.modulated_normalized_value()
+    }
+
+    /// NOTE: You need to call begin and end drag when using this
+    fn reset_param(&self) {
+        self.param_setter
+            .set_parameter(self.param, self.param.default_plain_value());
+    }
+
+    fn granular_drag(&self, ui: &Ui, drag_delta: Vec2) {
+        // Remember the intial position when we started with the granular drag. This value gets
+        // reset whenever we have a normal itneraction with the slider.
+        let start_value = if get_drag_amount_memory(ui) == 0.0 {
+            set_drag_normalized_start_value_memory(ui, self.normalized_value());
+            self.normalized_value()
+        } else {
+            get_drag_normalized_start_value_memory(ui)
+        };
+
+        let total_drag_distance = -drag_delta.y + get_drag_amount_memory(ui);
+        set_drag_amount_memory(ui, total_drag_distance);
+
+        self.set_normalized_value(
+            (start_value + (total_drag_distance * GRANULAR_DRAG_MULTIPLIER)).clamp(0.0, 1.0),
+        );
+    }
+
+    // Copied this to modify the normal drag behavior to not match a slider
+    fn normal_drag(&self, ui: &Ui, drag_delta: Vec2) {
+        let start_value = if get_drag_amount_memory(ui) == 0.0 {
+            set_drag_normalized_start_value_memory(ui, self.normalized_value());
+            self.normalized_value()
+        } else {
+            get_drag_normalized_start_value_memory(ui)
+        };
+
+        let total_drag_distance = -drag_delta.y + get_drag_amount_memory(ui);
+        set_drag_amount_memory(ui, total_drag_distance);
+
+        self.set_normalized_value(
+            (start_value + (total_drag_distance * NORMAL_DRAG_MULTIPLIER)).clamp(0.0, 1.0),
+        );
     }
 }
 
 impl<'a, P: Param> Widget for ArcKnob<'a, P> {
-    fn ui(mut self, ui: &mut Ui) -> Response {
+    fn ui(self, ui: &mut Ui) -> Response {
         // Figure out the size to reserve on screen for widget
         let bounding_box = Vec2::splat(self.size);
         let mut response = ui.allocate_response(bounding_box, Sense::click_and_drag());
 
-        let value = self.slider_region.handle_response(&ui, &mut response);
+        let value = {
+            if response.drag_started() {
+                // When beginning a drag or dragging normally, reset the memory used to keep track of
+                // our granular drag
+                self.param_setter.begin_set_parameter(self.param);
+                set_drag_amount_memory(ui, 0.0);
+            }
+            if let Some(_clicked_pos) = response.interact_pointer_pos() {
+                if ui.input(|mem| mem.modifiers.command) {
+                    // Like double clicking, Ctrl+Click should reset the parameter
+                    self.reset_param();
+                    response.mark_changed();
+                } else if ui.input(|mem| mem.modifiers.shift) {
+                    // And shift dragging should switch to a more granular input method
+                    self.granular_drag(ui, response.drag_delta());
+                    response.mark_changed();
+                } else {
+                    self.normal_drag(ui, response.drag_delta());
+                    response.mark_changed();
+                    //set_drag_amount_memory(ui, 0.0);
+                }
+            }
+            if response.double_clicked() {
+                self.reset_param();
+                response.mark_changed();
+            }
+            if response.drag_stopped() {
+                self.param_setter.end_set_parameter(self.param);
+                set_drag_amount_memory(ui, 0.0);
+            }
+            self.normalized_value()
+        };
 
         ui.vertical(|ui| {
             let painter = ui.painter_at(response.rect);
@@ -309,7 +282,7 @@ impl<'a, P: Param> Widget for ArcKnob<'a, P> {
             // Show hover text
             response
                 .clone()
-                .on_hover_text_at_pointer(self.slider_region.get_string());
+                .on_hover_text_at_pointer(self.param.to_string());
         });
         response
     }
