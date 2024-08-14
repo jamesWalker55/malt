9f3be94a0265025745fb535d51e53eddc3b54c16
James Walker <james.chunho@gmail.com>
2024-08-14 16:51:56 +0800

    create BiquadCoefficients to wrap coefficients

--- src/biquad.rs
+++ src/biquad.rs
@@ -1,13 +1,16 @@
 type Precision = f64;
 use std::f64::consts as C;
 
-pub(crate) struct Biquad {
-    b0: Precision,
-    b1: Precision,
-    b2: Precision,
-    a1: Precision,
-    a2: Precision,
+pub(crate) struct BiquadCoefficients {
+    pub(crate) b0: Precision,
+    pub(crate) b1: Precision,
+    pub(crate) b2: Precision,
+    pub(crate) a1: Precision,
+    pub(crate) a2: Precision,
+}
 
+pub(crate) struct Biquad {
+    coeff: BiquadCoefficients,
     // past input samples, (n - 1) and (n - 2)
     x1: Precision,
     x2: Precision,
@@ -17,19 +20,9 @@ pub(crate) struct Biquad {
 }
 
 impl Biquad {
-    pub(crate) fn new(
-        b0: Precision,
-        b1: Precision,
-        b2: Precision,
-        a1: Precision,
-        a2: Precision,
-    ) -> Self {
+    pub(crate) fn new(coeff: BiquadCoefficients) -> Self {
         Self {
-            b0,
-            b1,
-            b2,
-            a1,
-            a2,
+            coeff,
             x1: 0.0,
             x2: 0.0,
             u1: 0.0,
@@ -37,30 +30,20 @@ impl Biquad {
         }
     }
 
-    pub(crate) fn set_coefficients(
-        &mut self,
-        b0: Precision,
-        b1: Precision,
-        b2: Precision,
-        a1: Precision,
-        a2: Precision,
-    ) {
-        self.b0 = b0;
-        self.b1 = b1;
-        self.b2 = b2;
-        self.a1 = a1;
-        self.a2 = a2;
+    pub(crate) fn set_coefficients(&mut self, coeff: BiquadCoefficients) {
+        self.coeff = coeff;
     }
 
     pub(crate) fn is_stable(&self) -> bool {
         // |a1| < 2  &&  |a1| − 1 < a2 < 1
-        (self.a1.abs() < 2.0) && ((self.a1.abs() - 1.0) < self.a2 && self.a2 < 1.0)
+        (self.coeff.a1.abs() < 2.0)
+            && ((self.coeff.a1.abs() - 1.0) < self.coeff.a2 && self.coeff.a2 < 1.0)
     }
 
     pub(crate) fn process_sample(&mut self, x0: Precision) -> Precision {
-        let u0 = x0 * self.b0 + self.x1 * self.b1 + self.x2 * self.b2
-            - self.u1 * self.a1
-            - self.u2 * self.a2;
+        let u0 = x0 * self.coeff.b0 + self.x1 * self.coeff.b1 + self.x2 * self.coeff.b2
+            - self.u1 * self.coeff.a1
+            - self.u2 * self.coeff.a2;
 
         // clear sample if too low in volume
         // if u0 > -1e-10 && u0 < 1e-10 {
@@ -77,7 +60,7 @@ impl Biquad {
 }
 
 pub(crate) trait FixedQFilterKind {
-    fn coefficients(f: Precision, sr: Precision) -> [Precision; 5];
+    fn coefficients(f: Precision, sr: Precision) -> BiquadCoefficients;
 }
 
 pub(crate) struct FixedQFilter<T: FixedQFilterKind> {
@@ -95,7 +78,7 @@ impl<T: FixedQFilterKind> FixedQFilter<T> {
     pub(crate) fn new(frequency: Precision, sample_rate: Precision) -> Self {
         let coeffs = T::coefficients(frequency, sample_rate);
         Self {
-            biquad: Biquad::new(coeffs[0], coeffs[1], coeffs[2], coeffs[3], coeffs[4]),
+            biquad: Biquad::new(coeffs),
             f: frequency,
             sr: sample_rate,
             kind: std::marker::PhantomData,
@@ -104,8 +87,7 @@ impl<T: FixedQFilterKind> FixedQFilter<T> {
 
     fn update_coefficients(&mut self) {
         let coeffs = T::coefficients(self.f, self.sr);
-        self.biquad
-            .set_coefficients(coeffs[0], coeffs[1], coeffs[2], coeffs[3], coeffs[4]);
+        self.biquad.set_coefficients(coeffs);
     }
 
     pub(crate) fn set_frequency(&mut self, f: Precision) {
@@ -128,7 +110,7 @@ impl<T: FixedQFilterKind> FixedQFilter<T> {
 }
 
 pub(crate) trait GainlessFilterKind {
-    fn coefficients(f: Precision, q: Precision, sr: Precision) -> [Precision; 5];
+    fn coefficients(f: Precision, q: Precision, sr: Precision) -> BiquadCoefficients;
 }
 
 pub(crate) struct GainlessFilter<T: GainlessFilterKind> {
@@ -147,7 +129,7 @@ impl<T: GainlessFilterKind> GainlessFilter<T> {
     pub(crate) fn new(frequency: Precision, q: Precision, sample_rate: Precision) -> Self {
         let coeffs = T::coefficients(frequency, q, sample_rate);
         Self {
-            biquad: Biquad::new(coeffs[0], coeffs[1], coeffs[2], coeffs[3], coeffs[4]),
+            biquad: Biquad::new(coeffs),
             f: frequency,
             q,
             sr: sample_rate,
@@ -157,8 +139,7 @@ impl<T: GainlessFilterKind> GainlessFilter<T> {
 
     fn update_coefficients(&mut self) {
         let coeffs = T::coefficients(self.f, self.q, self.sr);
-        self.biquad
-            .set_coefficients(coeffs[0], coeffs[1], coeffs[2], coeffs[3], coeffs[4]);
+        self.biquad.set_coefficients(coeffs);
     }
 
     pub(crate) fn set_frequency(&mut self, f: Precision) {
@@ -192,7 +173,7 @@ impl<T: GainlessFilterKind> GainlessFilter<T> {
 pub(crate) struct ButterworthLP;
 
 impl FixedQFilterKind for ButterworthLP {
-    fn coefficients(f: Precision, sr: Precision) -> [Precision; 5] {
+    fn coefficients(f: Precision, sr: Precision) -> BiquadCoefficients {
         // Code from https://github.com/dimtass/DSP-Cpp-filters
         let c = 1.0 / (C::PI * f / sr).tan();
         let b0 = 1.0 / (1.0 + C::SQRT_2 * c + c.powi(2));
@@ -201,14 +182,14 @@ impl FixedQFilterKind for ButterworthLP {
         let a1 = 2.0 * b0 * (1.0 - c.powi(2));
         let a2 = b0 * (1.0 - C::SQRT_2 * c + c.powi(2));
 
-        [b0, b1, b2, a1, a2]
+        BiquadCoefficients { b0, b1, b2, a1, a2 }
     }
 }
 
 pub(crate) struct LinkwitzRileyLP;
 
 impl FixedQFilterKind for LinkwitzRileyLP {
-    fn coefficients(f: Precision, sr: Precision) -> [Precision; 5] {
+    fn coefficients(f: Precision, sr: Precision) -> BiquadCoefficients {
         // Code from https://github.com/dimtass/DSP-Cpp-filters
         let th = C::PI * f / sr;
         let wc = C::PI * f;
@@ -221,14 +202,14 @@ impl FixedQFilterKind for LinkwitzRileyLP {
         let a1 = (-2.0 * k.powi(2) + 2.0 * wc.powi(2)) / d;
         let a2 = (-2.0 * k * wc + k.powi(2) + wc.powi(2)) / d;
 
-        [b0, b1, b2, a1, a2]
+        BiquadCoefficients { b0, b1, b2, a1, a2 }
     }
 }
 
 pub(crate) struct LinkwitzRileyHP;
 
 impl FixedQFilterKind for LinkwitzRileyHP {
-    fn coefficients(f: Precision, sr: Precision) -> [Precision; 5] {
+    fn coefficients(f: Precision, sr: Precision) -> BiquadCoefficients {
         // Code from https://github.com/dimtass/DSP-Cpp-filters
         let th = C::PI * f / sr;
         let wc = C::PI * f;
@@ -241,14 +222,14 @@ impl FixedQFilterKind for LinkwitzRileyHP {
         let a1 = (-2.0 * k.powi(2) + 2.0 * wc.powi(2)) / d;
         let a2 = (-2.0 * k * wc + k.powi(2) + wc.powi(2)) / d;
 
-        [b0, b1, b2, a1, a2]
+        BiquadCoefficients { b0, b1, b2, a1, a2 }
     }
 }
 
 pub(crate) struct FirstOrderLP;
 
 impl FixedQFilterKind for FirstOrderLP {
-    fn coefficients(f: Precision, sr: Precision) -> [Precision; 5] {
+    fn coefficients(f: Precision, sr: Precision) -> BiquadCoefficients {
         // Code from https://github.com/dimtass/DSP-Cpp-filters
         let th = 2.0 * C::PI * f / sr;
         let g = th.cos() / (1.0 + th.sin());
@@ -258,14 +239,14 @@ impl FixedQFilterKind for FirstOrderLP {
         let a1 = -g;
         let a2 = 0.0;
 
-        [b0, b1, b2, a1, a2]
+        BiquadCoefficients { b0, b1, b2, a1, a2 }
     }
 }
 
 pub(crate) struct FirstOrderAP;
 
 impl FixedQFilterKind for FirstOrderAP {
-    fn coefficients(f: Precision, sr: Precision) -> [Precision; 5] {
+    fn coefficients(f: Precision, sr: Precision) -> BiquadCoefficients {
         // Code from https://github.com/dimtass/DSP-Cpp-filters
         let b = ((C::PI * f / sr).tan() - 1.0) / ((C::PI * f / sr).tan() + 1.0);
         let b0 = b;
@@ -274,14 +255,14 @@ impl FixedQFilterKind for FirstOrderAP {
         let a1 = b;
         let a2 = 0.0;
 
-        [b0, b1, b2, a1, a2]
+        BiquadCoefficients { b0, b1, b2, a1, a2 }
     }
 }
 
 pub(crate) struct CookbookLP;
 
 impl GainlessFilterKind for CookbookLP {
-    fn coefficients(f: Precision, q: Precision, sr: Precision) -> [Precision; 5] {
+    fn coefficients(f: Precision, q: Precision, sr: Precision) -> BiquadCoefficients {
         // code from https://github.com/robbert-vdh/nih-plug/blob/master/plugins/crossover/src/crossover/iir/biquad.rs
 
         let omega0 = C::TAU * (f / sr);
@@ -296,14 +277,14 @@ impl GainlessFilterKind for CookbookLP {
         let a1 = (-2.0 * cos_omega0) / a0;
         let a2 = (1.0 - alpha) / a0;
 
-        [b0, b1, b2, a1, a2]
+        BiquadCoefficients { b0, b1, b2, a1, a2 }
     }
 }
 
 pub(crate) struct CookbookHP;
 
 impl GainlessFilterKind for CookbookHP {
-    fn coefficients(f: Precision, q: Precision, sr: Precision) -> [Precision; 5] {
+    fn coefficients(f: Precision, q: Precision, sr: Precision) -> BiquadCoefficients {
         // code from https://github.com/robbert-vdh/nih-plug/blob/master/plugins/crossover/src/crossover/iir/biquad.rs
 
         let omega0 = C::TAU * (f / sr);
@@ -318,14 +299,14 @@ impl GainlessFilterKind for CookbookHP {
         let a1 = (-2.0 * cos_omega0) / a0;
         let a2 = (1.0 - alpha) / a0;
 
-        [b0, b1, b2, a1, a2]
+        BiquadCoefficients { b0, b1, b2, a1, a2 }
     }
 }
 
 pub(crate) struct CookbookAP;
 
 impl GainlessFilterKind for CookbookAP {
-    fn coefficients(f: Precision, q: Precision, sr: Precision) -> [Precision; 5] {
+    fn coefficients(f: Precision, q: Precision, sr: Precision) -> BiquadCoefficients {
         // code from https://github.com/robbert-vdh/nih-plug/blob/master/plugins/crossover/src/crossover/iir/biquad.rs
 
         let omega0 = C::TAU * (f / sr);
@@ -340,6 +321,6 @@ impl GainlessFilterKind for CookbookAP {
         let a1 = (-2.0 * cos_omega0) / a0;
         let a2 = (1.0 - alpha) / a0;
 
-        [b0, b1, b2, a1, a2]
+        BiquadCoefficients { b0, b1, b2, a1, a2 }
     }
 }
